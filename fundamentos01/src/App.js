d8c642210e0165087230ee6e37dfd9095e740ade
gcplayer000 <guilherme.camargo2013@hotmail.com>
2021-05-10 18:25:01 +0000

    aula01

---
+++ fundamentos01/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+   render(){
+     return(
+       <div>
+         <h1>Aula 01 - Fundamentos 01</h1> 
+       </div>
+     )
+     }
+    }
+    
+export default App;
